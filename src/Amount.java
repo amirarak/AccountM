4cbe4deb48b9fb40cb477f9a93cb9f205c7f1753
amirarak <145002376+amirarak@users.noreply.github.com>
2023-09-28 15:02:12 +0600

    Update Amount.java

--- src/Amount.java
+++ src/Amount.java
@@ -1,10 +1,20 @@
-public class Amount {    private double amount;
+package AccountM
+
+public class Amount
+{
+    private double amount;
     private String transactionType;
-    public Amount(double theamount, String theTransactionType)    {
-        amount = theamount;        transactionType = theTransactionType;
+    public Amount(double theamount, String theTransactionType)
+    {
+        amount = theamount;
+        transactionType = theTransactionType;
     }
-    public double getAmount()    {
-        return amount;    }
+    public double getAmount()
+    {
+        return amount;
+    }
     public String getTransactionType()
-    {        return transactionType;
-    }}
+    {
+        return transactionType;
+    }
+}
