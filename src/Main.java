eb81135b06da2b2c03c681966c77ffbbcf24a2ec
amirarak <145002376+amirarak@users.noreply.github.com>
2023-09-28 15:02:31 +0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,14 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+package AccountM// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 
-public class Main {    public Main() {
+public class Main {    public Main()
+{
+    public static void main(String[] args)
+    {
+        PersonalAccount amira = new PersonalAccount(220104031, "Amira");
+        new PersonalAccount(220104003, "Akylai");
+        amira.deposit(300.0);
+        amira.withdraw(200.0);
+        amira.printTransactionHistory();
+    }
 }
-    public static void main(String[] args) {        PersonalAccount amira = new PersonalAccount(220104031, "Amira");
-        new PersonalAccount(220104003, "Akylai");        amira.deposit(300.0);
-        amira.withdraw(200.0);        amira.printTransactionHistory();
-    }}
